1c49a6731e13b81d162baa9bd4b4fb49331f4bbc
oiaraque <oiaraque@gmail.com>
2022-04-13 19:11:30 -0500

    listCoin2.0

--- components/CoinItem.jsx
+++ components/CoinItem.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet, Image } from 'react-native';
 
 export default function CoinItem({coin}) {
 	return(
-		<View>
-			<Text>{coin.name}</Text>
+		<View style={styles.containerItems}>
+			<View style={styles.coinName}>
+				<Text style={styles.text}>{coin.name}</Text>
+				<Image
+					style={styles.image}  
+					source={{uri: coin.image}}
+				/>				
+			</View>
+			<Text style={styles.text}>${coin.current_price}</Text>
 		</View>
 
 	)
 }
+
+const styles = StyleSheet.create({
+	containerItems: {
+		backgroundColor: '#121212',
+		paddingTop: 10,
+		flexDirection: 'row',
+		justifyContent: 'space-between' 
+	},
+	coinName: {
+		flexDirection: 'row-reverse'
+	},
+	image: {
+		width: 30,
+		height: 30
+	},
+	text: {
+		color: '#ffffff'
+	}
+})
